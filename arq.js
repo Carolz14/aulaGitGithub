2c370814c255b24145474d56d4764fdc6dbedef7
Carolz14 <carolmbraga14@gmail.com>
2026-09-02 07:31:36 -0300

    Segundo commit do projeto: somar dois inteiros informados pelo usuario.

--- arq.js
+++ arq.js
@@ -1,6 +1,7 @@
 function soma () {
- var n1, n2;
+ var n1, n2, soma;
  n1 = document.getElementById ("primeiroNumero").value;
  n2 = document.getElementById ("segundoNumero").value;
  n1 = parseInt (n1);
+ n2 = parseInt (n2);
 }
